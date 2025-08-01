44eb70fce4fa579dcc181a5be00219ed4c8f5c52
yokthanwa1993 <yokthanwa1993@gmail.com>
2025-08-01 17:10:35 +0700

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 export default defineConfig(() => ({
   server: {
     host: "::",
     port: 32100,
   },
-  plugins: [react()],
+  plugins: [dyadComponentTagger(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
